a9e8629a26d725b0da913104d583833d4d58205f
SebastianGP4 <163374024+SebastianGP4@users.noreply.github.com>
2024-04-25 09:04:55 -0600

    Add files via upload

---
+++ CajeroATM/Form1.Designer.cs
@@ -0,0 +1,369 @@
+﻿
+namespace CajeroATM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.boton0 = new System.Windows.Forms.Button();
+            this.enter = new System.Windows.Forms.Button();
+            this.boton9 = new System.Windows.Forms.Button();
+            this.boton8 = new System.Windows.Forms.Button();
+            this.boton4 = new System.Windows.Forms.Button();
+            this.boton7 = new System.Windows.Forms.Button();
+            this.boton6 = new System.Windows.Forms.Button();
+            this.boton5 = new System.Windows.Forms.Button();
+            this.boton3 = new System.Windows.Forms.Button();
+            this.boton2 = new System.Windows.Forms.Button();
+            this.boton1 = new System.Windows.Forms.Button();
+            this.cancelar = new System.Windows.Forms.Button();
+            this.borrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cuenta = new System.Windows.Forms.TextBox();
+            this.nip = new System.Windows.Forms.TextBox();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // boton0
+            // 
+            this.boton0.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.boton0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton0.ForeColor = System.Drawing.Color.Blue;
+            this.boton0.Location = new System.Drawing.Point(6, 133);
+            this.boton0.Name = "boton0";
+            this.boton0.Size = new System.Drawing.Size(37, 32);
+            this.boton0.TabIndex = 10;
+            this.boton0.Text = "0";
+            this.boton0.UseVisualStyleBackColor = false;
+            // 
+            // enter
+            // 
+            this.enter.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.enter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enter.ForeColor = System.Drawing.Color.Blue;
+            this.enter.Location = new System.Drawing.Point(49, 133);
+            this.enter.Name = "enter";
+            this.enter.Size = new System.Drawing.Size(80, 32);
+            this.enter.TabIndex = 1;
+            this.enter.Text = "ENTER";
+            this.enter.UseVisualStyleBackColor = false;
+            // 
+            // boton9
+            // 
+            this.boton9.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.boton9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton9.ForeColor = System.Drawing.Color.Blue;
+            this.boton9.Location = new System.Drawing.Point(92, 95);
+            this.boton9.Name = "boton9";
+            this.boton9.Size = new System.Drawing.Size(37, 32);
+            this.boton9.TabIndex = 11;
+            this.boton9.Text = "9";
+            this.boton9.UseVisualStyleBackColor = false;
+            // 
+            // boton8
+            // 
+            this.boton8.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.boton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton8.ForeColor = System.Drawing.Color.Blue;
+            this.boton8.Location = new System.Drawing.Point(49, 95);
+            this.boton8.Name = "boton8";
+            this.boton8.Size = new System.Drawing.Size(37, 32);
+            this.boton8.TabIndex = 12;
+            this.boton8.Text = "8";
+            this.boton8.UseVisualStyleBackColor = false;
+            // 
+            // boton4
+            // 
+            this.boton4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.boton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton4.ForeColor = System.Drawing.Color.Blue;
+            this.boton4.Location = new System.Drawing.Point(6, 57);
+            this.boton4.Name = "boton4";
+            this.boton4.Size = new System.Drawing.Size(37, 32);
+            this.boton4.TabIndex = 13;
+            this.boton4.Text = "4";
+            this.boton4.UseVisualStyleBackColor = false;
+            // 
+            // boton7
+            // 
+            this.boton7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.boton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton7.ForeColor = System.Drawing.Color.Blue;
+            this.boton7.Location = new System.Drawing.Point(6, 95);
+            this.boton7.Name = "boton7";
+            this.boton7.Size = new System.Drawing.Size(37, 32);
+            this.boton7.TabIndex = 14;
+            this.boton7.Text = "7";
+            this.boton7.UseVisualStyleBackColor = false;
+            // 
+            // boton6
+            // 
+            this.boton6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.boton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton6.ForeColor = System.Drawing.Color.Blue;
+            this.boton6.Location = new System.Drawing.Point(92, 57);
+            this.boton6.Name = "boton6";
+            this.boton6.Size = new System.Drawing.Size(37, 32);
+            this.boton6.TabIndex = 15;
+            this.boton6.Text = "6";
+            this.boton6.UseVisualStyleBackColor = false;
+            // 
+            // boton5
+            // 
+            this.boton5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.boton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton5.ForeColor = System.Drawing.Color.Blue;
+            this.boton5.Location = new System.Drawing.Point(49, 57);
+            this.boton5.Name = "boton5";
+            this.boton5.Size = new System.Drawing.Size(37, 32);
+            this.boton5.TabIndex = 16;
+            this.boton5.Text = "5";
+            this.boton5.UseVisualStyleBackColor = false;
+            // 
+            // boton3
+            // 
+            this.boton3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.boton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton3.ForeColor = System.Drawing.Color.Blue;
+            this.boton3.Location = new System.Drawing.Point(92, 19);
+            this.boton3.Name = "boton3";
+            this.boton3.Size = new System.Drawing.Size(37, 32);
+            this.boton3.TabIndex = 17;
+            this.boton3.Text = "3";
+            this.boton3.UseVisualStyleBackColor = false;
+            // 
+            // boton2
+            // 
+            this.boton2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.boton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton2.ForeColor = System.Drawing.Color.Blue;
+            this.boton2.Location = new System.Drawing.Point(49, 19);
+            this.boton2.Name = "boton2";
+            this.boton2.Size = new System.Drawing.Size(37, 32);
+            this.boton2.TabIndex = 18;
+            this.boton2.Text = "2";
+            this.boton2.UseVisualStyleBackColor = false;
+            // 
+            // boton1
+            // 
+            this.boton1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.boton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton1.ForeColor = System.Drawing.Color.Blue;
+            this.boton1.Location = new System.Drawing.Point(6, 19);
+            this.boton1.Name = "boton1";
+            this.boton1.Size = new System.Drawing.Size(37, 32);
+            this.boton1.TabIndex = 19;
+            this.boton1.Text = "1";
+            this.boton1.UseVisualStyleBackColor = false;
+            // 
+            // cancelar
+            // 
+            this.cancelar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.cancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelar.ForeColor = System.Drawing.Color.Blue;
+            this.cancelar.Location = new System.Drawing.Point(135, 44);
+            this.cancelar.Name = "cancelar";
+            this.cancelar.Size = new System.Drawing.Size(59, 32);
+            this.cancelar.TabIndex = 20;
+            this.cancelar.Text = "X";
+            this.cancelar.UseVisualStyleBackColor = false;
+            // 
+            // borrar
+            // 
+            this.borrar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.borrar.Font = new System.Drawing.Font("Wingdings 3", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.borrar.ForeColor = System.Drawing.Color.Blue;
+            this.borrar.Location = new System.Drawing.Point(135, 82);
+            this.borrar.Name = "borrar";
+            this.borrar.Size = new System.Drawing.Size(59, 32);
+            this.borrar.TabIndex = 21;
+            this.borrar.Text = "3";
+            this.borrar.UseVisualStyleBackColor = false;
+            this.borrar.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.GhostWhite;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(15, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(615, 182);
+            this.label1.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox2.Controls.Add(this.cancelar);
+            this.groupBox2.Controls.Add(this.borrar);
+            this.groupBox2.Controls.Add(this.boton1);
+            this.groupBox2.Controls.Add(this.boton2);
+            this.groupBox2.Controls.Add(this.boton5);
+            this.groupBox2.Controls.Add(this.boton3);
+            this.groupBox2.Controls.Add(this.boton6);
+            this.groupBox2.Controls.Add(this.boton4);
+            this.groupBox2.Controls.Add(this.boton9);
+            this.groupBox2.Controls.Add(this.boton8);
+            this.groupBox2.Controls.Add(this.boton7);
+            this.groupBox2.Controls.Add(this.boton0);
+            this.groupBox2.Controls.Add(this.enter);
+            this.groupBox2.Location = new System.Drawing.Point(15, 209);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 170);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(239, 209);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(391, 76);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(239, 303);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(391, 76);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Bienvenido";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(236, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Digita tu número de cuenta";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(40, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(214, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Digita tu nip (4 números)";
+            // 
+            // cuenta
+            // 
+            this.cuenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cuenta.Location = new System.Drawing.Point(282, 54);
+            this.cuenta.Name = "cuenta";
+            this.cuenta.Size = new System.Drawing.Size(223, 38);
+            this.cuenta.TabIndex = 8;
+            // 
+            // nip
+            // 
+            this.nip.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nip.Location = new System.Drawing.Point(282, 107);
+            this.nip.MaxLength = 4;
+            this.nip.Name = "nip";
+            this.nip.PasswordChar = '*';
+            this.nip.Size = new System.Drawing.Size(223, 38);
+            this.nip.TabIndex = 9;
+            this.nip.UseSystemPasswordChar = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(642, 405);
+            this.Controls.Add(this.nip);
+            this.Controls.Add(this.cuenta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cajero ATM";
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button boton0;
+        private System.Windows.Forms.Button enter;
+        private System.Windows.Forms.Button boton9;
+        private System.Windows.Forms.Button boton8;
+        private System.Windows.Forms.Button boton4;
+        private System.Windows.Forms.Button boton7;
+        private System.Windows.Forms.Button boton6;
+        private System.Windows.Forms.Button boton5;
+        private System.Windows.Forms.Button boton3;
+        private System.Windows.Forms.Button boton2;
+        private System.Windows.Forms.Button boton1;
+        private System.Windows.Forms.Button cancelar;
+        private System.Windows.Forms.Button borrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox cuenta;
+        private System.Windows.Forms.TextBox nip;
+    }
+}
+
---
+++ CajeroATM/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CajeroATM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            cuenta.Clear();
+            nip.Clear();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
